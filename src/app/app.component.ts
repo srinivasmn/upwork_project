57d1f3f2e64ae92fda322115fcf205d434e49c4c
Srinivas <srinivasmnath@gmail.com>
2019-02-17 19:33:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,103 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'upwork-proj';
+  //title = 'upwork-proj';
+  count: number = 0;
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+          label: 'Safety Management',
+          icon: 'pi pi-fw pi-file',
+          items: [{
+                  label: 'New', 
+                  icon: 'pi pi-fw pi-plus',
+                  items: [
+                      {label: 'Project'},
+                      {label: 'Other'},
+                  ]
+              },
+              {label: 'Open'},
+              {separator:true},
+              {label: 'Quit'}
+          ]
+      },
+      {
+          label: 'Construction manage',
+          icon: 'pi pi-fw pi-pencil',
+          items: [
+              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+          ]
+      },
+      {
+          label: 'Education management',
+          icon: 'pi pi-fw pi-question',
+          items: [
+              {
+                  label: 'Contents'
+              },
+              {
+                  label: 'Search', 
+                  icon: 'pi pi-fw pi-search', 
+                  items: [
+                      {
+                          label: 'Text', 
+                          items: [
+                              {
+                                  label: 'Workspace'
+                              }
+                          ]
+                      },
+                      {
+                          label: 'File'
+                      }
+              ]}
+          ]
+      },
+      {
+          label: 'Material management',
+          icon: 'pi pi-fw pi-cog',
+          items: [
+              {
+                  label: 'Edit',
+                  icon: 'pi pi-fw pi-pencil',
+                  items: [
+                      {label: 'Save', icon: 'pi pi-fw pi-save'},
+                      {label: 'Update', icon: 'pi pi-fw pi-save'},
+                  ]
+              },
+              {
+                  label: 'Other',
+                  icon: 'pi pi-fw pi-tags',
+                  items: [
+                      {label: 'Delete', icon: 'pi pi-fw pi-minus'}
+                  ]
+              }
+          ]
+      },
+      {
+        label: 'Administration menu',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {label: 'Save', icon: 'pi pi-fw pi-save'},
+                    {label: 'Update', icon: 'pi pi-fw pi-save'},
+                ]
+            },
+            {
+                label: 'Other',
+                icon: 'pi pi-fw pi-tags',
+                items: [
+                    {label: 'Delete', icon: 'pi pi-fw pi-minus'}
+                ]
+            }
+        ]
+    }
+    ];
+  }
 }
